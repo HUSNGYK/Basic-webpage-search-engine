d54ead771b1523b2d06d62b2bbd031f1bbec3bc2
mikekenndy <mishoal11@gmail.com>
2018-11-24 18:57:55 -0500

    First step in reading files

--- src/WebGraph.java
+++ src/WebGraph.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collection;
 
 //Mike Kennedy
@@ -10,11 +15,34 @@ public class WebGraph
 	private Collection<WebPage> pages;
 	private int[][] edges = new int[MAX_PAGES][MAX_PAGES];
 	
-	public WebGraph() {}
+	public WebGraph() 
+	{
+		
+	}
 	
 	public static WebGraph buildFromFiles(String pagesFile, String linksFile)
-	throws IllegalArgumentException	//TODO: throw exception if file does not reference valid file
-	{
+	throws IllegalArgumentException, FileNotFoundException, IOException
+	//TODO: throw exception if file does not reference valid file
+	//TODO: throw fileNotFoundException if file doesn't exist
+	//TODO: throw IOException if file is empty
+	{		
+		FileInputStream pagesIn = new FileInputStream(pagesFile);
+		InputStreamReader pagesInStream = new InputStreamReader(pagesIn);
+		BufferedReader pagesReader = new BufferedReader(pagesInStream);
+		
+		String currentLine = pagesReader.readLine();
+		while(currentLine != null)
+		{
+			System.out.println(currentLine);
+			currentLine = pagesReader.readLine();
+		}
+		
+		
+		
+		FileInputStream linksIn = new FileInputStream(linksFile);
+		InputStreamReader linksInStream = new InputStreamReader(linksIn);
+		BufferedReader linksReader = new BufferedReader(linksInStream);
+		
 		return null;		
 	}
 	public void addPage(String url, Collection<String> keywords)
